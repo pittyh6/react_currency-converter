6f6dd141b2f67b001bf6fc385b9b288b7dc136c1
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-07-22 09:58:52 +1000

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(["Have a good time", "Travel every year"]);
+  const [todoValue, setTodoValue] = useState("");
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
@@ -16,11 +17,23 @@ function App() {
     });
     setTodos(newTodoList);
   }
-  function handleEditTodo(index) {}
+  function handleEditTodo(index) {
+    const valueToBeEdit = todos[index];
+    setTodoValue(valueToBeEdit);
+    handleDeleteTodo(index);
+  }
   return (
     <>
-      <TodoInput handleAddTodos={handleAddTodos} />
-      <TodoList todos={todos} handleDeleteTodo={handleDeleteTodo} />
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+      />
     </>
   );
 }
